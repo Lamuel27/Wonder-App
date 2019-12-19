bccb25e03b6527064dcd841acc571993fa627661
Lamuel27 <samrelliner@gmail.com>
2019-12-19 10:07:25 -0600

    added custom sharing

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -630,6 +630,25 @@
         sr.reveal( '[data-reveal="right"]', rightReveal );
     }
     
+    function share(){
+  
+        var shareButtons = document.querySelectorAll(".share-btn");
+      
+        if (shareButtons) {
+            [].forEach.call(shareButtons, function(button) {
+            button.addEventListener("click", function(event) {
+                       var width = 650,
+                  height = 450;
+      
+              event.preventDefault();
+      
+              window.open(this.href, 'Share Dialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width='+width+',height='+height+',top='+(screen.height/2-height/2)+',left='+(screen.width/2-width/2));
+            });
+          });
+        }
+      
+      }
+
     $(document).ready(function () {
         scrollAnimation();
         $.when(heroContentSlider()).then(heroContentSliderFade());
@@ -656,6 +675,7 @@
         countdown();
         vossenBlogGrid();
         lightbox();
+        share();
     });
     
     
